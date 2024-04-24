f5b09fb6cf3b392f6884da48c2ec3c376404b254
NouraMohamed123 <nouramohamed091@gmail.com>
2024-04-24 21:40:27 +0200

    gg

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('photo')->nullable();
             $table->string('password');
             $table->string('roles_name')->nullable();
+            $table->integer('type')->default(1);
             $table->timestamps();
         });
     }
