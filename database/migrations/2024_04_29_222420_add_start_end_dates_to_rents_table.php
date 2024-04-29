e3edfe1b278aa6db6620ac7267b15e486e874819
AhmedHany852 <ahanyebrahim@gmail.com>
2024-04-30 02:20:29 +0300

    l

---
+++ database/migrations/2024_04_29_222420_add_start_end_dates_to_rents_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('rents', function (Blueprint $table) {
+            $table->date('start_date')->nullable()->after('amount'); // Adding nullable start date column after the amount column
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->date('end_date')->nullable()->after('start_date'); // Adding nullable end date column after the start date column
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('rents', function (Blueprint $table) {
+            //
+        });
+    }
+};
